7b0a6dec3a1005ebe7f4f9fee9e32fa773a6fc5e
ashokboya891 <ashokboya891@gmail.com>
2023-11-21 18:21:31 +0530

    end of sec 8 getting data  from api to client and showing in webpage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { retryWhen } from 'rxjs';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+  products:Product[]=[];
+  constructor(private http:HttpClient) {
+    
+  }
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
+    next:response=>this.products=response.data,
+    error:error=>console.log(error),
+    complete:()=>{
+      console.log('request done');
+
+      
+    }
+    
+    
+      
+  })
+  }
 }
