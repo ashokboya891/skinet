94fa54573d799c35f98d84de6a7e2aacada8957d
ashokboya891 <ashokboya891@gmail.com>
2023-11-24 18:09:48 +0530

    end of sec-9 designing,child component for items,communication b/w compo,filters,sorting,pagination as shared  with output funtionality,seraching added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { retryWhen } from 'rxjs';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
+
 
 @Component({
   selector: 'app-root',
@@ -11,22 +9,23 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit{
   title = 'Skinet';
-  products:Product[]=[];
-  constructor(private http:HttpClient) {
+
+  constructor() {
     
   }
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
-    next:response=>this.products=response.data,
-    error:error=>console.log(error),
-    complete:()=>{
-      console.log('request done');
+  //   this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
+  //   next:response=>this.products=response.data,
+
+  //   error:error=>console.log(error),
+  //   complete:()=>{
+  //     console.log('request done');
 
       
-    }
+  //   }
     
     
       
-  })
+  // })
   }
 }
---
+++ client/src/app/shared/paging-header/paging-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-paging-header',
+  templateUrl: './paging-header.component.html',
+  styleUrls: ['./paging-header.component.scss']
+})
+export class PagingHeaderComponent {
+  @Input() pageNumber?:number;
+  @Input() pageSize?:number;
+  @Input() totalCount?:number;
+
+  
+
+}
